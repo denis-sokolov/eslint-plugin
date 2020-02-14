3c07323c6c6c0af31c97675b907618503e356adf
Denis Sokolov <denis@sokolov.cc>
2020-02-14 12:00:00 +0200

    configs: remove typescrint-eslint recommended ruleset as not preferred
    Overall, the judgements made by that ruleset do not align with our judgements.
    This commit removes the ruleset and manually selects some rules to keep.

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -12,11 +12,12 @@ const minimal = {
   },
   plugins: ["@typescript-eslint", "react-hooks"],
   rules: {
-    "@typescript-eslint/no-extra-non-null-assertion": ["error"],
-    "@typescript-eslint/no-empty-interface": ["error"],
-    "@typescript-eslint/no-namespace": ["error"],
-    "@typescript-eslint/no-non-null-asserted-optional-chain": ["error"],
-    "@typescript-eslint/no-unused-expressions": ["error"],
+    "@typescript-eslint/no-extra-non-null-assertion": "error",
+    "@typescript-eslint/no-empty-interface": "error",
+    "@typescript-eslint/no-misused-new": "error",
+    "@typescript-eslint/no-namespace": "error",
+    "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
+    "@typescript-eslint/no-unused-expressions": "error",
     "@typescript-eslint/triple-slash-reference": [
       "error",
       { path: "never", types: "never", lib: "never" }
@@ -52,46 +53,28 @@ const recommended = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended"
   ],
   rules: {
     ...minimal.rules,
 
-    "@theorem/no-double-negative-variables": ["error"],
+    "@theorem/no-double-negative-variables": "error",
     "@theorem/no-enums": "error",
+    "@theorem/no-non-null-assertion": "error",
     "@theorem/no-positional-boolean-parameters": "error",
     "@theorem/no-react-key-prefix": "error",
     "@theorem/no-todos": "error",
 
-    // Override with our rules
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@theorem/no-non-null-assertion": "error",
-
-    // See below for no-explicit-any
-    "@typescript-eslint/ban-ts-ignore": "off",
-    // Opinionated
-    "@typescript-eslint/camelcase": "off",
-    // Opinionated take on return type inferrence
-    "@typescript-eslint/explicit-function-return-type": "off",
-    // Opinionated about plentiful no-op functions
-    "@typescript-eslint/no-empty-function": "off",
-    // Any is an important tool for less experienced developers
-    // and for dynamic untyped functions
-    "@typescript-eslint/no-explicit-any": "off",
-    // While questionable, f && f() is a popular false positive
-    "@typescript-eslint/no-unused-expressions": [
-      "error",
-      { allowShortCircuit: true }
-    ],
-    // TypeScript already finds this, and the rule needs configuring to understand _ prefix
-    "@typescript-eslint/no-unused-vars": "off",
-    // Enforces a particular order of function declarations in a module
-    "@typescript-eslint/no-use-before-define": "off",
+    // This modifies eslint no-unused-vars with ts awareness
+    "@typescript-eslint/no-unused-vars": "error",
     // Not enough benefit
     "react/no-children-prop": "off",
     // Irrelevant with TypeScript
-    "react/prop-types": "off"
+    "react/prop-types": "off",
+
+    "no-var": "error",
+    "prefer-rest-params": "error",
+    "prefer-spread": "error"
   },
   settings: { react: { version: "detect" } }
 };
@@ -100,9 +83,18 @@ const opinionated = {
   ...recommended,
   rules: {
     ...recommended.rules,
-    "@typescript-eslint/prefer-optional-chain": ["error"],
-    "prefer-const": "error",
-    "react-hooks/exhaustive-deps": "error"
+
+    "@typescript-eslint/adjacent-overload-signatures": "error",
+    "@typescript-eslint/consistent-type-assertions": [
+      "error",
+      { assertionStyle: "as" }
+    ],
+    "@typescript-eslint/no-inferrable-types": "error",
+    "@typescript-eslint/prefer-optional-chain": "error",
+
+    "react-hooks/exhaustive-deps": "error",
+
+    "prefer-const": "error"
   }
 };
 
