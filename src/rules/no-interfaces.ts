3053ec888c41aabc0d5745e50940f1fe3d1a05da
Denis Sokolov <denis@sokolov.cc>
2020-05-14 12:00:00 +0300

    no-interfaces: typo

--- src/rules/no-interfaces.ts
+++ src/rules/no-interfaces.ts
@@ -6,7 +6,7 @@ const rule: Rule = {
       TSInterfaceDeclaration(node) {
         context.report({
           message: [
-            "Avoid using interface keyword, use tye instead: type Size = { height: number }.",
+            "Avoid using interface keyword, use type instead: type Size = { height: number }.",
             "Interface keyword and type keyword define exactly the same types, this is purely a syntactic distinction.",
             "Where you would use Foo extends Bar { ... }, use Foo = Bar & ...",
           ].join(" "),
