a7c27479b3095094b75d2fa54be757f46755e058
Denis Sokolov <denis@sokolov.cc>
2020-10-28 12:00:00 +0000

    rule: no-imports-up improve message

--- src/rules/no-imports-up.ts
+++ src/rules/no-imports-up.ts
@@ -15,6 +15,7 @@ const rule: Rule = {
             "Avoid importing up from the current level.",
             "The convention is dependency tree is represented by directory hierarchy, so importing up means creating an import cycle.",
             "Consider instead passing data that you want to import as an explicit parameter into the current module.",
+            'If the module you are importing does not depend on the current module, consider instead to expose it using a path alias to make it look like a module, e.g. `import {} from "src/buttons";`',
           ].join(" "),
           node,
         });
