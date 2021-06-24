1f20f56bdd1543343f200f81f2f5582f3fdeab16
Denis Sokolov <denis@sokolov.cc>
2021-06-24 12:00:00 +0000

    rules: tests for no-non-null-assertion

---
+++ src/rules/no-non-null-assertion.test.ts
@@ -0,0 +1,16 @@
+import { invalid, valid } from "../tester";
+import rule from "./no-non-null-assertion";
+
+[
+  "a.foo",
+  "a(foo)?.bar",
+].forEach((code, i) =>
+  valid(`no-non-null-assertion valid ${i}`, rule, code)
+);
+
+[
+  "a!.foo",
+  "a(foo)!",
+].forEach((code, i) =>
+  invalid(`no-non-null-assertion found in ${i}`, rule, code)
+);
