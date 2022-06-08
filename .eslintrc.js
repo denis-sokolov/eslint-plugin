9429de96fbca4e315ccdd288f7a5746534615916
Denis Sokolov <denis@sokolov.cc>
2022-06-08 12:00:00 +0000

    dependencies: reintroduce denis-sokolov/eslint-plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [],
-  plugins: [],
+  extends: ["plugin:@denis-sokolov/opinionated"],
+  plugins: ["@denis-sokolov"],
   rules: {
+    // Importing a lot of implementation details from eslint package
+    "@denis-sokolov/no-imports-down": "off",
+
     // Our tests include string samples of code
     "no-template-curly-in-string": "off",
   },
