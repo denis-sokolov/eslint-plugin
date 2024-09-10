24bc7aa6a89de1bcfd2ba96e2271dd9a90a57a21
Denis Sokolov <denis@sokolov.cc>
2024-09-10 12:00:00 +0000

    eslint: allow defining meta on rules

--- src/eslint/types.ts
+++ src/eslint/types.ts
@@ -1,6 +1,7 @@
 import type {
   RuleContext,
   RuleListener,
+  RuleMetaData,
 } from "@typescript-eslint/utils/dist/ts-eslint";
 import type { Location, Node } from "../ast";
 
@@ -16,7 +17,10 @@ export type Context = Omit<RuleContext<MessageIds, void[]>, "report"> & {
   report: (params: ReportParams) => void;
 };
 
-export type Rule = { create: (context: Context) => RuleListener };
+export type Rule = {
+  create: (context: Context) => RuleListener;
+  meta?: Partial<RuleMetaData<string>>;
+};
 
 export type Rules = {
   [name: string]: Rule;
