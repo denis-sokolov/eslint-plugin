4d6f9b4c808e65f4017b40a974daba92b7a1fb22
Denis Sokolov <denis@sokolov.cc>
2025-02-13 12:00:00 +0000

    extra: filter and reword are now generic and preserve the original types

--- src/eslint/changeRule.ts
+++ src/eslint/changeRule.ts
@@ -2,10 +2,10 @@ import { type Context, type Rule } from "./types";
 
 type Params = Parameters<Context["report"]>[0] & { data?: unknown };
 
-export function changeReport(
-  base: Rule,
+export function changeReport<Base extends Rule>(
+  base: Base,
   f: (params: Params & { report: Context["report"] }) => void,
-): Rule {
+): Base {
   return changeRule(base, function (context) {
     const p: typeof context = Object.create(context, {
       report: { writable: true },
@@ -17,7 +17,10 @@ export function changeReport(
   });
 }
 
-export function changeRule(base: Rule, f: Rule["create"]): Rule {
+export function changeRule<Base extends Rule>(
+  base: Base,
+  f: Rule["create"],
+): Base {
   return {
     ...base,
     create: function (context) {
@@ -26,16 +29,19 @@ export function changeRule(base: Rule, f: Rule["create"]): Rule {
   };
 }
 
-export function filter(base: Rule, f: (params: Params) => boolean) {
+export function filter<Base extends Rule>(
+  base: Base,
+  f: (params: Params) => boolean,
+): Base {
   return changeReport(base, function (params) {
     if (f({ ...params })) params.report(params);
   });
 }
 
-export function reword(
-  base: Rule,
+export function reword<Base extends Rule>(
+  base: Base,
   messageOrFunction: string | ((params: Params) => string),
-) {
+): Base {
   const f =
     typeof messageOrFunction === "string"
       ? () => messageOrFunction
