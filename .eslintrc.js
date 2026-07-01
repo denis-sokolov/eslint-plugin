6550e9ab2de793d75ab615c54e19d19fef4561e9
Denis Sokolov <denis@sokolov.cc>
2026-07-01 12:00:00 +0000

    lint: remove self-dependency for the upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,5 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:@denis-sokolov/opinionated"],
-  plugins: ["@denis-sokolov"],
-  rules: {
-    // Importing a lot of implementation details from eslint package
-    "@denis-sokolov/no-imports-down": "off",
-
-    // Our tests include string samples of code
-    "no-template-curly-in-string": "off",
-  },
+  rules: {},
 };
